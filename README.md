701eb7cc8e4bdc4bcd9c349c0afc4e928400ab5b
michaelkoukoui <michaelkoukoui5@gmail.com>
2025-06-22 12:40:43 +0300

    first working version of base extension

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# gandlaf README
+# gandalf README
 
-This is the README for your extension "gandlaf". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "gandalf". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gandlaf" is now active!');
+	console.log('Congratulations, your extension "gandalf" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gandlaf.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('gandalf.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Gandlaf!');
+		vscode.window.showInformationMessage('Hello World from gandalf!');
 	});
 
 	context.subscriptions.push(disposable);
